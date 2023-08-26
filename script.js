4f825b7f0aa80059f8e876bc9ec74b95066c79bb
Rahul Yadav <119389899+Yadav123Rahul@users.noreply.github.com>
2023-08-26 11:35:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function volume_sphere() {
     //Write your code here
+	 // Get the radius input value from the form
+    let radius = parseFloat(document.getElementById('radius').value);
+
+    // Calculate the volume of the sphere (V = 4/3 * π * r^3)
+    let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+    // Display the calculated volume in the volume field
+    document.getElementById('volume').value = volume.toFixed(2);
   
 } 
 
